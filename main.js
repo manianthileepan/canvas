30175dd6092a1d3678ff443708a3d3fb18fc8539
manianthileepan <95533063+manianthileepan@users.noreply.github.com>
2022-04-05 11:32:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function new_image(){
+    fabric.image.fromURL('BirthdayImage,jpg',function(Img){
+        block_image_object = Img;
+        
+        block_image_object.scaleToHeight(700)
+        block_image_object.scaleToHeight(510)
+        block_image_object.set({
+        top:0,
+        left:0
+        });
+        CanvasGradient.add(block_image_object);
+    });
+
+}
+function  Playsound(){
+    X.Play();
+}
